b6d6a7f1efdf72f74d93543af5cdf20fde129916
KWON-WOO <ckw075@naver.com>
2025-10-14 15:11:20 +0900

    add solution2 function and comments

--- src/codekata/Codekata.java
+++ src/codekata/Codekata.java
@@ -7,8 +7,8 @@ public class Codekata {
         int selectSolution;
         Scanner solutionSc = new Scanner(System.in);
         Scanner sc = new Scanner(System.in);
-         do{
-             System.out.print("솔루션 선택->");
+        do {
+            System.out.print("솔루션 선택->");
             selectSolution = solutionSc.nextInt();
             switch (selectSolution) {
                 case 0:
@@ -16,11 +16,19 @@ public class Codekata {
                 case 1:
                     System.out.println(solution1(sc.nextInt(), sc.nextInt()));
                     break;
+                case 2:
+                    System.out.println(solution2(sc.nextInt(), sc.nextInt()));
+                    break;
                 default:
             }
-        }while (selectSolution != 0);
+        } while (selectSolution != 0);
     }
-    public static int solution1(int num1, int num2) {
+
+    public static int solution1(int num1, int num2) {   //두 수의 차 구하기
         return num1 - num2;
+    }
+
+    public static int solution2(int num1, int num2) {   // 두 수의 곱 구하기
+        return num1 * num2;
     }
 }
