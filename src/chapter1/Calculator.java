eaa4ed206f8d1c2902aefbb8cda56bc00b9dcbba
KWON-WOO <ckw075@naver.com>
2025-10-17 19:10:08 +0900

    Calculator.java에 코멘트 추가.

--- src/chapter1/Calculator.java
+++ src/chapter1/Calculator.java
@@ -15,6 +15,10 @@ public class Calculator {
 //        resultListIndex = -1;   //이후 배열에 추가할 때 [++index]방식을 이용하기 위해 초기값을 -1로 설정
     }
 
+    /**
+     * 함수 실행부분. 실행의 모든 부분을 관리함. 첫 입력값을 String으로 받아와 조건검사까지 모두 시행한다.
+     * @return
+     */
     public boolean execute() {
         Scanner sc = new Scanner(System.in);
         boolean loopContinue = true;
@@ -42,7 +46,6 @@ public class Calculator {
                 if (!resultList.isEmpty())
                     removeResult();
                 break;
-
             default:
                 num1 = Integer.parseInt(num1Str);
                 System.out.print("두번째 숫자 입력->");
@@ -62,6 +65,15 @@ public class Calculator {
         return loopContinue;
     }
 
+    /**
+     * 연산이 실행되는 부분.
+     * 나눗셈의 경우 2번째 피연산자 num2가 0일 경우 별도의 출력문 출력.
+     * 연산자가 아닌 문자가 들어왔을 경우에 결과값을 출력하지 않도록 boolean값을 세팅.
+     * @param num1 피연산자1
+     * @param num2 피연산자2
+     * @param operator 연산자
+     * @return
+     */
     public int calculate(int num1, int num2, char operator) { //실제로 연산에 들어가는 메서드.
         int result = 0;
         switch (operator) {
@@ -88,14 +100,22 @@ public class Calculator {
 
         return result;
     }
-
+    /**
+     * getter 함수.
+     * 결과값이 담기는 resultList의 List를 출력해주는 함수.
+     * 별도의 매개변수를 필요로 하지 않고 반환값 없이 콘솔에서만 출력한다.
+     */
     public void getResultList() {
         System.out.println("결과값");
         for (int i = 0; i <= resultList.size(); i++) {
             System.out.println(i + ". " + resultList.get(i));
         }
     }
-
+    /**
+     * 결과값이 담기는 setResultList의 값을 수정하는 함수.
+     * 수정을 위해 수정할 결과값이 들어있는 인덱스와 수정할 값을 받아온다.
+     * 별도의 매개변수 없이 함수 내에서 매개변수를 모두 받는다.
+     */
     public void setResultList() {
         Scanner sc = new Scanner(System.in);
         int index;
@@ -114,7 +134,10 @@ public class Calculator {
         resultList.set(index, result);
         System.out.println("수정완료됨");
     }
-
+    /**
+     * 결과값을 지워주는 함수.
+     * 무조건 제일 먼저 들어온 값을 지우도록 설계함.
+     */
     public void removeResult() {
         resultList.remove(0);
 //        int result;           //조건에 맞게 수정함.
