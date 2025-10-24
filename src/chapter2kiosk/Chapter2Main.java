68369fb0aad22734776204f6649df206f5943c4f
KWON-WOO <ckw075@naver.com>
2025-10-24 14:24:08 +0900

    chapter2Main 간소화.

--- src/chapter2kiosk/Chapter2Main.java
+++ src/chapter2kiosk/Chapter2Main.java
@@ -3,14 +3,9 @@ package chapter2kiosk;
 import java.util.Scanner;
 
 public class Chapter2Main {
-    int selectLevel;
-    Scanner sc;
     Kiosk kiosk;
-    Menu menu;
     public Chapter2Main(){
-        this.menu = new Menu("Burger Menu");
         this.kiosk = new Kiosk();
-        this.sc = new Scanner(System.in);
     }
     public void execute() {
         kiosk.start();
